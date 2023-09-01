e5781429011c4dd683d3cc065c5e729a628b304b
Sean Walsh <seanwalsh@hey.com>
2023-09-01 12:34:02 +0300

    feat: update sha

--- useless-go-cli.rb
+++ useless-go-cli.rb
@@ -2,7 +2,7 @@ class UselessGoCli < Formula
   desc "A useless CLI tool"
   homepage "https://github.com/seancwalsh/useless-go-cli.git"
   url "https://github.com/seancwalsh/useless-go-cli/archive/refs/tags/V1.0.0.tar.gz"
-  sha256 "d061263a93ca28570584f075a7b6a29f5ebb3f258b32167e3c6708ea16b7e2dc"
+  sha256 "a33a665f44499b5f943cf746bfed2f6d8d3ae4896ec922dd4e7ffdf6efb51c49"
   license "MIT"
 
   def install
