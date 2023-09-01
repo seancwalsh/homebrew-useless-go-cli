8b43f587f0110e0a583c25cd2687b13c4a97f58e
Sean Walsh <seanwalsh@hey.com>
2023-09-01 12:39:01 +0300

    fix: wrong tar link

--- useless-go-cli.rb
+++ useless-go-cli.rb
@@ -1,7 +1,7 @@
 class UselessGoCli < Formula
   desc "A useless CLI tool"
   homepage "https://github.com/seancwalsh/useless-go-cli.git"
-  url "https://github.com/seancwalsh/useless-go-cli/archive/refs/tags/V1.0.0.tar.gz"
+  url "https://github.com/seancwalsh/useless-go-cli/archive/refs/tags/V1.0.1.tar.gz"
   sha256 "a33a665f44499b5f943cf746bfed2f6d8d3ae4896ec922dd4e7ffdf6efb51c49"
   license "MIT"
 
