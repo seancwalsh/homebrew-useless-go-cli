4fb8c5294b30d82f43ef78c0514cf76af1e4a63b
Sean Walsh <seanwalsh@hey.com>
2023-09-01 12:18:05 +0300

    feat: homebrew formula

---
+++ useless-go-cli.rb
@@ -0,0 +1,16 @@
+class UselessGoCli < Formula
+  desc "A useless CLI tool"
+  homepage "https://github.com/seancwalsh/useless-go-cli.git"
+  url "https://github.com/seancwalsh/useless-go-cli/archive/refs/tags/V1.0.0.tar.gz"
+  sha256 "d061263a93ca28570584f075a7b6a29f5ebb3f258b32167e3c6708ea16b7e2dc"
+  license "MIT"
+
+  def install
+    bin.install "simple-cli-tool"
+  end
+
+  test do
+    system "#{bin}/simple-cli-tool", "--version"
+  end
+end
+
